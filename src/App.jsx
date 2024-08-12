9693c3ee9437a75957790249a2183e3b9ad30e95
SanhindaKL <118117722+sanga-kl@users.noreply.github.com>
2024-08-12 13:20:22 +0530

    commit-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,15 @@
-import Market from "./Market.jsx"
-import Header from "./Header.jsx"
-import Card from "./Card.jsx"
-import NavigationBar from "./NavigationBar.jsx"
-import Additional from "./Additional.jsx"
-import Partner from "./Partners.jsx"
-import Footer from "./Footer.jsx"
 
+import Form from "./Form"
+import { Dashboard } from "./Dashboard"
 function App() {
-
 
   return (
     <>
-      {/* <Header/><Food/><Footer/><hr />
-      <Card/><Card/><Card/><Card/><hr />
-      <Button/><Button/><hr /><br />
-      <Student name="Sachin" age={23} isStudent={true}/>
-      <Student name="pasindu" age={24} isStudent={false}/>
-      <Student name="pasindu" age={24} isStudent={false}/>
-      <Student name="pasindu" age={24} isStudent={false}/>
-      <Student name="pasindu" age={24} isStudent={false}/>
-      <hr />
-      <UserGreeting isLoggedIn={false} name="brocode"/>
-      <UserGreeting isLoggedIn={false}/><hr /> */}
-      <NavigationBar/>
-     <br />
-      <Header/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <br />
-      <hr />
-      <Market/><hr />
-      <Additional/>
-      <br /><hr />
-      <Partner/>
-      <br />
-      <Footer/>
-      <br />
-      <hr />
-
-
-
-
+    <Form/>
+    <br />
+    <Dashboard/>
     </>
-  );
+  )
 }
 
 export default App
